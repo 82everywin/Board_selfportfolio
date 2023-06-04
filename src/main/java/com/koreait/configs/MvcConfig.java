ec59c8a3209dd9ca57a8724197bd6dab80b3ab60
82everywin <82everywin@gmail.com>
2023-06-05 03:46:50 +0900

    스프링 시큐리티 기본 설정. 정적 경로 설정. 임시 메인 페이지 추가

---
+++ src/main/java/com/koreait/configs/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.koreait.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload.path}")
+    private String fileUploadPath;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+         registry.addResourceHandler("/uploads/**" )
+                 .addResourceLocations("file:///"+fileUploadPath);
+    }
+
+    /**
+     * 설정에 대한 틀 제공
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("main/index");
+    }
+
+}
