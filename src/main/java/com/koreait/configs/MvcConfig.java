c862c7de17367e47da1ec80a785507e0b883d0f5
82everywin <82everywin@gmail.com>
2023-06-07 02:08:09 +0900

    html 기본 설정

--- src/main/java/com/koreait/configs/MvcConfig.java
+++ src/main/java/com/koreait/configs/MvcConfig.java
@@ -1,16 +1,23 @@
 package com.koreait.configs;
 
+import com.koreait.configs.interceptors.SiteConfigInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
 
     @Value("${file.upload.path}")
     private String fileUploadPath;
+
+    // 사이트 설정 유지
+    private final SiteConfigInterceptor siteConfigInterceptor;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
          registry.addResourceHandler("/uploads/**" )
@@ -27,4 +34,9 @@ public class MvcConfig implements WebMvcConfigurer {
                 .setViewName("main/index");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**"); // 모든 URL에 해당.
+    }
 }
