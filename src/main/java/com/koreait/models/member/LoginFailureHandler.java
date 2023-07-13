09fd33c454caa1c9d9b6416a32d4bdf370bb22a9
82everywin <82everywin@gmail.com>
2023-07-13 22:25:24 +0900

    로그인 세션 처리

--- src/main/java/com/koreait/models/member/LoginFailureHandler.java
+++ src/main/java/com/koreait/models/member/LoginFailureHandler.java
@@ -19,18 +19,29 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
         String userId= request.getParameter("userId");
         String userPw= request.getParameter("userPw");
 
+        session.removeAttribute("userId");
+        session.removeAttribute("requiredUserId");
+        session.removeAttribute("requiredUserPw");
+        session.removeAttribute("global");
+
+
+        session.setAttribute("userId",userId);
+
         try{
             if (userId == null || userId.isBlank()){
-                throw new LoginValidationException("userId","NotBlank.userId");
+                throw new LoginValidationException("requiredUserId","NotBlank.userId");
             }
 
             if (userPw == null || userPw.isBlank()){
-                throw new LoginValidationException("userPw","NotBlank.userPw");
+                throw new LoginValidationException("requiredUserPw","NotBlank.userPw");
             }
 
             throw new LoginValidationException("global","validation.login.fail");
+
         }catch(LoginValidationException e){
             session.setAttribute(e.getField(),e.getMessage());
         }
+
+        response.sendRedirect(request.getContextPath() + "/member/login");
     }
 }
