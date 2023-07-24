624259cb87ae47121267eb4e5c4c463d35db9c2c
82everywin <82everywin@gmail.com>
2023-07-24 23:23:30 +0900

    관리자 기본설정 구성중

---
+++ src/main/java/com/koreait/repositories/ConfigsRepository.java
@@ -0,0 +1,9 @@
+package com.koreait.repositories;
+
+
+import com.koreait.entities.Configs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfigsRepository extends JpaRepository<Configs,String> {
+
+}
